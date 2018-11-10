b5bb270b9658e756b970c4da77c76e440ac7b7eb
Harvey Li <aide0740@gmail.com>
2018-11-10 13:08:27 -0500

    stage

--- drawShips.js
+++ drawShips.js
@@ -1,4 +1,23 @@
 
+latestTime = new Date(1945,7,15);
+earliestTime = new Date(1941,11,7);
+timeRange = Math.round((latestTime-earliestTime)/60/60/1000/24);
+timeSlider = document.getElementById("timeSlider");
+timeSlider.max = timeRange;
+timeSlider.value = 0;
+currentTime = earliestTime;
+
+let timeScaler = d3.scaleTime()
+    .domain([earliestTime,latestTime])
+    .range([0, 800]);
+let timeSliderAxis= d3.axisBottom(timeScaler);
+axis = d3.select("#Axis");
+axis.append("svg")
+    .call(timeSliderAxis.tickFormat(d3.timeFormat("%Y-%m")))
+    .style('width',820)
+    .style('height',30)
+
+
 var projection = d3.geoMercator()
 .center([0, 25 ])
 .scale(250)
@@ -10,12 +29,18 @@ var path = d3.geoPath()
 
 
 d3.json("ships.json", function(error, data) {
-    latestTime = new Date(1800,0,1);
-    earliestTime = new Date(2018,0,1);
     for (ship of data){
         for (event of ship.events){
-
+            tempDate = new Date(event.date);
+            if (tempDate>latestTime) latestTime = tempDate;
+            if (tempDate<earliestTime) earliestTime = tempDate;
         }
     }
-
+    console.log(latestTime);
+    console.log(earliestTime);
+    
 });
+
+function setTime(time){
+    currentTime = new Date(earliestTime.getTime() + time * 60*60*1000*24);
+}
